2cd4fb205c7f828bb815efe5cf9b742da5a5099c
Oliver Guntli <oliverguntli@gmail.com>
2016-07-16 15:46:41 +0200

    Controller written to increase and decrease grumpyness, incl. cool down

--- grantometer/controllers.py
+++ grantometer/controllers.py
@@ -1,6 +1,11 @@
+import dateutil.parser
+
+
 class GrumpyControl:
     def __init__(self):
         self.grumpyness = 0
+        self.last_action = None
+        self.timestamp = None
 
     def increase_grumpyness(self):
         if self.grumpyness > 500:
@@ -10,8 +15,27 @@ class GrumpyControl:
         else:
             self.grumpyness = 1
 
-    def decrease_grumpyness():
-        pass
+    def decrease_grumpyness(self):
+        if self.grumpyness > 0 and self.grumpyness <= 1000:
+            self.grumpyness = self.grumpyness - 50
+        elif self.grumypness > 1000:
+            self.grumpyness = 950
+
+    def cool_down_grumpyness(self, now):
+        action_timestamp = dateutil.parser.parse(now)
+        delta_t = action_timestamp - self.timestamp
+        print(delta_t)
+        if delta_t.days > 0 or delta_t.seconds > 43200:
+            self.grumpyness = 0
+        elif delta_t.seconds > 39600:
+            self.grumpyness = 1
+        else:
+            houres = delta_t.seconds // 3600
+            new_grumpyness = self.grumpyness - houres * (10 + houres)
+            if new_grumpyness > 0:
+                self.grumpyness = new_grumpyness
+            else:
+                self.grumpyness = 2
 
     def query_grumpyness():
         pass
--- test/controller_test.py
+++ test/controller_test.py
@@ -1,5 +1,6 @@
 import unittest
 from grantometer import controllers
+import dateutil.parser
 
 
 class ControllerTests(unittest.TestCase):
@@ -13,10 +14,44 @@ class ControllerTests(unittest.TestCase):
         gt = controllers.GrumpyControl()
         gt.grumpyness = 255
         gt.increase_grumpyness()
-        self.assertEqual(510, gt.grumpyness, 'Grumpyness from 510')
+        self.assertEqual(510, gt.grumpyness, 'Grumpyness from 501')
 
     def test_increase_grumpyness_to_max(self):
         gt = controllers.GrumpyControl()
         gt.grumpyness = 501
         gt.increase_grumpyness()
-        self.assertEqual(1000, gt.grumpyness, 'Grumpyness from 510')
+        self.assertEqual(1000, gt.grumpyness, 'Grumpyness to 1000')
+
+    def test_decrease_grumpyness(self):
+        gt = controllers.GrumpyControl()
+        gt.grumpyness = 800
+        gt.decrease_grumpyness()
+        self.assertEqual(750, gt.grumpyness)
+
+    def test_cool_down_3h(self):
+        gt = controllers.GrumpyControl()
+        gt.grumpyness = 1000
+        gt.timestamp = dateutil.parser.parse("2016-07-16T13:00:00Z")
+        gt.cool_down_grumpyness("2016-07-16T16:00:00Z")
+        self.assertEqual(961, gt.grumpyness)
+
+    def test_cool_down_12h00m(self):
+        gt = controllers.GrumpyControl()
+        gt.grumpyness = 1000
+        gt.timestamp = dateutil.parser.parse("2016-07-16T04:00:00Z")
+        gt.cool_down_grumpyness("2016-07-16T16:00:00Z")
+        self.assertEqual(1, gt.grumpyness)
+
+    def test_cool_down_24h00(self):
+        gt = controllers.GrumpyControl()
+        gt.grumpyness = 1000
+        gt.timestamp = dateutil.parser.parse("2016-07-15T16:00:00Z")
+        gt.cool_down_grumpyness("2016-07-16T16:00:00Z")
+        self.assertEqual(0, gt.grumpyness)
+
+    def test_cool_down_7h00(self):
+        gt = controllers.GrumpyControl()
+        gt.grumpyness = 100
+        gt.timestamp = dateutil.parser.parse("2016-07-15T06:00:00Z")
+        gt.cool_down_grumpyness("2016-07-15T16:00:00Z")
+        self.assertEqual(2, gt.grumpyness)
