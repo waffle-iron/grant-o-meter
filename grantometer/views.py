291baca48948ff044025b1c346d3d45ca21e66a2
Oliver Guntli <oliverguntli@gmail.com>
2016-08-17 19:41:56 +0200

    Bugfix on cool down algo

--- grantometer/views.py
+++ grantometer/views.py
@@ -8,9 +8,6 @@ import datetime
 
 @app.route('/setup')
 def setup():
-    db.create_all()
-    db.session.add(models.Grumpiness(1))
-    db.session.commit()
     return "S'all good man"
 
 
@@ -21,9 +18,9 @@ def main_gauge(name=None):
 
 @app.route('/grumpy/api/v1_0', methods=['GET', 'POST'])
 def manage_grumpiness():
+    timestamp = datetime.datetime.now()
     gc = db.session.query(models.Grumpiness).order_by(
                        models.Grumpiness.id.desc()).first()
-    timestamp = datetime.datetime.now()
     grumpiness  = controllers.cool_down_grumpiness(gc.grumpiness,
                         timestamp,
                         gc.timestamp)
@@ -32,19 +29,24 @@ def manage_grumpiness():
         try:
             action = data['action']
             if action == 'decrease':
+                print("decrease: %s" % gc.grumpiness)
                 grumpiness = controllers.decrease_grumpiness(grumpiness)
+                print("to: %s" % grumpiness)
             elif action == 'increase':
-
+                print("increase from: %s" % gc.grumpiness)
                 grumpiness = controllers.increase_grumpiness(grumpiness)
-                print(grumpiness)
+                print("to: %s" % grumpiness)
         except KeyError:
             return jsonify(error="Action ist missing in" +
                                   " your request")
         except:
             return jsonify(error="Whooopsie Daysie")
-        db.session.add(models.Grumpiness(grumpiness))
+        new_entry = models.Grumpiness(grumpiness, timestamp)
+        db.session.add(new_entry)
         db.session.commit()
+        print(new_entry)
     elif request.method == 'GET':
-        db.session.add(models.Grumpiness(grumpiness))
-        db.session.commit()
+        print("GET")
+        print('Get request: %s at %s - %s' % (grumpiness, gc.timestamp, timestamp) )
     return jsonify(grumpiness=grumpiness)
+
